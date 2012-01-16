8025ecb9c53df625812d0a8d5af1c55e8084647f
Senthil Kumar <senthil.vijay.kumar@gmail.com>
2012-01-16 14:50:50 +0530

    Working WCF Recieve Locations

---
+++ WcfClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using WcfClient.Purchase;
+
+namespace WcfClient
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Operation_1Request purchaseRequest = new Operation_1Request(new PurchaseRequest
+                                                                            {
+                                                                                customerId = 5,
+                                                                                productId = 1,
+                                                                                quantity = 3
+                                                                            });
+            PurchaseOrchestrations_PurchaseOrchestartion_PurchasePort client = new PurchaseOrchestrations_PurchaseOrchestartion_PurchasePortClient("BasicHttpBinding_ITwoWayAsync");
+            var response = client.Operation_1(purchaseRequest);
+            Console.Read();
+        }
+    }
+}
--- WcfClient/Properties/AssemblyInfo.cs
+++ WcfClient/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PurchaseExternalServices")]
+[assembly: AssemblyTitle("WcfClient")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PurchaseExternalServices")]
+[assembly: AssemblyProduct("WcfClient")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("401bc06c-62ca-4701-9684-47ad84ba17b2")]
+[assembly: Guid("640dfb5d-8d41-4b08-8fb0-567060e6321f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
